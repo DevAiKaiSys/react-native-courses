39fb6cbdc01ad1e60093cafb4303aaa19dbad465
DevAiKaiSys <dev.aikaisys@gmail.com>
2026-01-24 23:29:00 +0700

    Custom tabBarIcon

--- README.md
+++ README.md
@@ -42,5 +42,5 @@ module.exports = {
 Snipped
 
 ```
-rnfes
+rnfe
 ```
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,4 @@
 import {Text, View} from "react-native";
-import {Link} from "expo-router";
 
 export default function Index() {
     return (
@@ -7,8 +6,6 @@ export default function Index() {
             <Text className="text-5xl text-dark-200 font-bold">
                 Welcome!
             </Text>
-            <Link href="/onboarding">Onboarding</Link>
-            <Link href="/movie/avangers">Avenger Movie</Link>
         </View>
     );
 }
