c3019260438a60b4b9f0ad6c6ce23bb842fbf80b
DevAiKaiSys <dev.aikaisys@gmail.com>
2026-01-25 16:58:31 +0700

    Develop Search Screen

--- services/useFetch.ts
+++ services/useFetch.ts
@@ -27,8 +27,8 @@ const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
 
     const reset = () => {
         setData(null);
-        setLoading(false);
         setError(null);
+        setLoading(false);
     }
 
     useEffect(() => {
