80f9b391ad4dd9d731203a9de95135a8041594d4
DevAiKaiSys <dev.aikaisys@gmail.com>
2026-01-30 22:39:15 +0700

    Animated Text Reveal (Moti + Reanimated)

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,13 +5,33 @@ import { COLORS, FONT_FAMILY, homeTitle } from '../constants'
 import { MotiView } from 'moti'
 
 const HomeScreen = () => {
+  /* const animatedTitle = [...homeTitle.split(' '), '"'].filter(
+    word => word !== '"',
+  ); */
+  const animatedTitle = homeTitle.trim().split(/\s+/);
+
   return (
     <SafeAreaView style={styles.container}>
       {/* Title */}
-      <View>
-        <MotiView>
-          <Text style={styles.titleText}>{homeTitle}</Text>
-        </MotiView>
+      <View style={styles.titleContainer}>
+        {animatedTitle.map((text, index) => (
+          <MotiView
+            key={index}
+            from={{
+              opacity: 0,
+              translateY: 10,
+            }}
+            animate={{
+              opacity: 1,
+              translateY: 0,
+            }}
+            transition={{
+              type: 'spring',
+              delay: index * 250,
+            }}>
+            <Text style={styles.titleText}>{text}</Text>
+          </MotiView>
+        ))}
       </View>
       <Text>HomeScreen</Text>
     </SafeAreaView>
@@ -21,14 +41,21 @@ const HomeScreen = () => {
 export default HomeScreen
 
 const styles = StyleSheet.create({
-   container: {
+  container: {
     flex: 1,
     backgroundColor: COLORS.primaryWhite,
   },
-    titleText: {
+  titleText: {
     fontSize: 28,
     fontFamily: FONT_FAMILY.poppins_semibold,
     color: COLORS.primaryBlack,
     paddingLeft: 8,
+  },
+  titleContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    rowGap: 3,
+    flexWrap: 'wrap',
+    paddingHorizontal: 20,
   },
 })
